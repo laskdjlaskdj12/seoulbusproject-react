61515275cf6e3a38f630e5c643212cc72663f23c
laskdjlaskdj12 <laskdjlaskdj12@gmail.com>
2020-09-14 04:50:08 +0900

    laskdjOS-Front 프로젝에서 갖고온 기능들을 이식

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {CookiesProvider} from "react-cookie";
+import {BrowserRouter} from "react-router-dom";
+import './css/root.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <CookiesProvider>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </CookiesProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
